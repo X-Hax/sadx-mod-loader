ede56801f235b005b74f31eb9ad101efe8a0238f
Michael Fadely <michaelwfadely@gmail.com>
2017-03-20 12:20:59 -0600

    Added update URL fields to the New Mod Dialog

--- SADXModManager/Forms/NewModDialog.cs
+++ SADXModManager/Forms/NewModDialog.cs
@@ -16,51 +16,55 @@ namespace SADXModManager.Forms
 		{
 			string moddir = Path.Combine(Path.Combine(Environment.CurrentDirectory, "mods"), ValidateFilename(textModName.Text));
 
-			if (textModName.Text.Length > 0)
-			{
-				try
-				{
-					if (!Directory.Exists(moddir))
-					{
-						Directory.CreateDirectory(moddir);
-
-						if (checkRedirectMainSave.Checked || checkRedirectChaoSave.Checked)
-							Directory.CreateDirectory(Path.Combine(moddir, "SAVEDATA"));
-
-						if (checkOpenFolder.Checked)
-							System.Diagnostics.Process.Start(moddir);
-
-						ModInfo newMod = new ModInfo
-						{
-							Name				= textModName.Text,
-							Author				= textModAuthor.Text,
-							Description			= textModDescription.Text,
-							Version				= textVersion.Text,
-							RedirectMainSave	= checkRedirectMainSave.Checked,
-							RedirectChaoSave	= checkRedirectChaoSave.Checked
-						};
-
-						IniSerializer.Serialize(newMod, Path.Combine(moddir, "mod.ini"));
-
-						// I feel like this is a little hackish, but it works.
-						// Setting it on the button causes it to close regardless
-						// of whether or not you call Close(), which we don't want.
-						DialogResult = DialogResult.OK;
-						Close();
-					}
-					else
-					{
-						MessageBox.Show("A mod with that name already exists.\nPlease choose a different name or rename the existing one.", "Mod already exists", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-					}
-				}
-				catch (Exception error)
-				{
-					MessageBox.Show(this, error.Message, "Mod Creation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-			}
-			else
+			if (textModName.Text.Length <= 0)
 			{
 				MessageBox.Show("You can't have a mod without a name.", "Invalid mod name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				if (Directory.Exists(moddir))
+				{
+					MessageBox.Show("A mod with that name already exists."
+					                + "\nPlease choose a different name or rename the existing one.", "Mod already exists",
+						MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+					return;
+				}
+
+				Directory.CreateDirectory(moddir);
+
+				if (checkRedirectMainSave.Checked || checkRedirectChaoSave.Checked)
+				{
+					Directory.CreateDirectory(Path.Combine(moddir, "SAVEDATA"));
+				}
+
+				ModInfo newMod = new ModInfo
+				{
+					Name             = textModName.Text,
+					Author           = textModAuthor.Text,
+					Description      = textModDescription.Text,
+					Version          = textVersion.Text,
+					RedirectMainSave = checkRedirectMainSave.Checked,
+					RedirectChaoSave = checkRedirectChaoSave.Checked,
+					GitHubRepo       = textGitHubRepo.Text,
+					GitHubAsset      = textGitHubAttachment.Text,
+					UpdateUrl        = textDirUrl.Text
+				};
+
+				IniSerializer.Serialize(newMod, Path.Combine(moddir, "mod.ini"));
+
+				if (checkOpenFolder.Checked)
+				{
+					System.Diagnostics.Process.Start(moddir);
+				}
+
+				DialogResult = DialogResult.OK;
+				Close();
+			}
+			catch (Exception error)
+			{
+				MessageBox.Show(this, error.Message, "Mod Creation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 
