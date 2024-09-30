5c1066e246f88d2f4aab18bccb5ba0aa5918af77
PiKeyAr <pikeyar@pm.me>
2024-09-30 09:30:26 +0900

    New patches system

--- SADXModLoader/config.h
+++ SADXModLoader/config.h
@@ -3,3 +3,5 @@
 void LoadModLoaderSettings(LoaderSettings* loaderSettings, std::wstring appPath);
 unsigned int GetModCount();
 std::string GetModName(int index);
+bool IsGamePatchEnabled(const char* patchName);
+void ListPatches();
